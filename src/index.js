854cf786157582f93b09d7e30df68c8896063436
Maxi Ferreira <charca@gmail.com>
2016-01-29 11:53:39 -0800

    Added webpack config

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import DeviceRedirect from './device-redirect'
+'use strict'
 
-((define) => {
-  return define(() => DeviceRedirect)
-})(((undefined) => {
-  if (module && module.exports) {
-    return (factory) => { module.exports = factory() }
-  } else if (define && define.amd) {
-    return define
-  } else if (window !== undefined) {
-    return (factory) => { window.DeviceRedirect = factory() }
-  } else {
-    throw new Error('Unknown Environment. Please file a bug if you get this error.')
+const MobileDetect = require('mobile-detect')
+const MOBILE_TIMEOUT = 500
+
+class DeviceRedirect {
+  constructor(conditions, userAgent) {
+    this.conditions = conditions
+    this.detect = new MobileDetect(userAgent)
   }
-})())
+
+  openApp() {
+    let condition = this.conditions[this.detect.os()]
+    if (!condition) {
+      return;
+    }
+
+    let iframe = document.createElement('iframe')
+    iframe.setAttribute('src', condition.urls.appUrl)
+    iframe.style.display = 'none'
+    document.body.appendChild(iframe)
+    this.timeout = setTimeout(() => {
+      document.location = condition.urls.redirectUrl
+    }, MOBILE_TIMEOUT)
+    if(window && window.removeEventListener) {
+      window.addEventListener('pagehide', this._preventPopup)
+    }
+  }
+
+  _preventPopup() {
+    clearTimeout(this.timeout)
+    this.timeout = null
+    if(window && window.removeEventListener) {
+      window.removeEventListener('pagehide', this._preventPopup)
+    }
+  }
+}
+
+export default DeviceRedirect
